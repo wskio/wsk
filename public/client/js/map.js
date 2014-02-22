d261b71d7ee7680472632c5725d4d073ea8b12f5
redwoodfavorite <joseph.michael.sample@gmail.com>
2014-02-22 02:42:47 -0800

    added radius to maps

--- public/client/js/map.js
+++ public/client/js/map.js
@@ -4,7 +4,7 @@ var mapInit = function(){
   var siberia = new google.maps.LatLng(60, 105);
   var newyork = new google.maps.LatLng(40.69847032728747, -73.9514422416687);
   var browserSupportFlag =  new Boolean();
-
+  var radiusCircle;
   var myOptions = {
     zoom: 18,
     mapTypeId: google.maps.MapTypeId.ROADMAP
@@ -24,8 +24,19 @@ var mapInit = function(){
         position: initialLocation,
         title: 'Click to zoom'
       });
-
+      var radiusOptions = {
+        strokeColor: '#FF0000',
+        strokeOpacity: 0.8,
+        strokeWeight: 2,
+        fillColor: '#FF0000',
+        fillOpacity: 0.35,
+        map: map,
+        center: initialLocation,
+        radius: 50,
+      };
+      radiusCircle = new google.maps.Circle(radiusOptions);
       marker.setMap(map);
+
     }, function() {
       handleNoGeolocation(browserSupportFlag);
     });
@@ -102,3 +113,4 @@ function setMarkers(map, locations) {
     });
   }
 }
+

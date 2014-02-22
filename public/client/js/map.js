24897fe25313b462a4318583cebfe8ebab8447a3
brianlk <brian.kang2@gmail.com>
2014-02-21 17:23:13 -0800

    added comments

--- public/client/js/map.js
+++ public/client/js/map.js
@@ -6,10 +6,11 @@ var mapInit = function(){
   };
   var map = new google.maps.Map(document.getElementById("map-canvas"),
       mapOptions);
+  // adds marker at position defined below
   var marker = new google.maps.Marker({
     position: map.getCenter(),
     map: map,
     title: 'Click to zoom'
   });
-  google.maps.event.addDomListener(window, 'load', initialize);
+  google.maps.event.addDomListener(window, 'load', "initialize");
 }
